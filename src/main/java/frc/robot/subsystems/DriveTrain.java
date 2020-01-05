e1b14ec9f6fec873926fed18a6951652323e308f
Noah Stephen Haskell <nstephenh@gmail.com>
2020-01-04 17:27:49 -0800

    Six motor drive train

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -21,7 +21,11 @@ public class DriveTrain extends SubsystemBase {
    */
   
     private final CANSparkMax m_leftMotor;
+    private final CANSparkMax m_leftMotor_1;
+    private final CANSparkMax m_leftMotor_2;
     private final CANSparkMax m_rightMotor;
+    private final CANSparkMax m_rightMotor_1;
+    private final CANSparkMax m_rightMotor_2;
     /*
     private final PWMSparkMax m_leftMotor;
     private final PWMSparkMax m_rightMotor;
@@ -32,16 +36,36 @@ public class DriveTrain extends SubsystemBase {
     super();
     
     m_leftMotor = new CANSparkMax(5, MotorType.kBrushless);
-    m_rightMotor = new CANSparkMax(6, MotorType.kBrushless);
+    m_leftMotor_1 = new CANSparkMax(6, MotorType.kBrushless);
+    m_leftMotor_2 = new CANSparkMax(7, MotorType.kBrushless);
+
+    m_rightMotor = new CANSparkMax(8, MotorType.kBrushless);
+    m_rightMotor_1 = new CANSparkMax(9, MotorType.kBrushless);
+    m_rightMotor_2 = new CANSparkMax(10, MotorType.kBrushless);
+
     /*
     m_leftMotor = new PWMSparkMax(0);
     m_rightMotor = new PWMSparkMax(1);
     */
     m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
-    
+
+    m_leftMotor.restoreFactoryDefaults();
     m_leftMotor.getEncoder(EncoderType.kHallSensor, 4096);
-    m_rightMotor.getEncoder(EncoderType.kHallSensor, 4096);
+    m_leftMotor_1.restoreFactoryDefaults();
+    m_leftMotor_1.getEncoder(EncoderType.kHallSensor, 4096);
+    m_leftMotor_1.follow(m_leftMotor);
+    m_leftMotor_2.restoreFactoryDefaults();
+    m_leftMotor_2.getEncoder(EncoderType.kHallSensor, 4096);
+    m_leftMotor_2.follow(m_leftMotor);
     
+    m_rightMotor.restoreFactoryDefaults();
+    m_rightMotor.getEncoder(EncoderType.kHallSensor, 4096);
+    m_rightMotor_1.restoreFactoryDefaults();
+    m_rightMotor_1.getEncoder(EncoderType.kHallSensor, 4096);
+    m_rightMotor_1.follow(m_rightMotor);
+    m_rightMotor_2.restoreFactoryDefaults();
+    m_rightMotor_2.getEncoder(EncoderType.kHallSensor, 4096);
+    m_rightMotor_2.follow(m_rightMotor);
 
   }
   public void drive(double left, double right){
