7f02732e7810765375b3dd1926a9b47d2187bd8a
David B <djbrook4@ncsu.edu>
2020-01-06 22:59:50 -0500

    Vision for color

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ import frc.robot.commands.ManualPowerCell;
 import frc.robot.subsystems.ControlPanel;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.PowerCell;
+import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.Elevator;
 
 /**
@@ -40,6 +41,7 @@ public class RobotContainer {
   final Elevator m_elevator = new Elevator();
   final PowerCell m_powercell = new PowerCell();
   final ControlPanel controlPanel = new ControlPanel();
+  final Vision vision = new Vision();
 
   // Configure the button bindings
   final Joystick controller = new Joystick(0);
@@ -70,8 +72,6 @@ public class RobotContainer {
    */
   public RobotContainer() {
 
-
-    CameraServer.getInstance().startAutomaticCapture();
 
 
 
